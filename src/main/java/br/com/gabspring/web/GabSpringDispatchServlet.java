5cee5de1af864539369df5e67115a250ac36f199
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2025-03-23 02:27:45 -0300

    feat: Melhoria no manuseio de solicitações com invocação e log do controlador dinâmico

--- src/main/java/br/com/gabspring/web/GabSpringDispatchServlet.java
+++ src/main/java/br/com/gabspring/web/GabSpringDispatchServlet.java
@@ -1,10 +1,15 @@
 package br.com.gabspring.web;
 
+import br.com.gabspring.datastructures.ControllerInstances;
+import br.com.gabspring.datastructures.ControllersMap;
+import br.com.gabspring.util.GabLogger;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
 
 public class GabSpringDispatchServlet extends HttpServlet {
 
@@ -16,7 +21,41 @@ public class GabSpringDispatchServlet extends HttpServlet {
             return;
         }
 
-        System.out.println("Received request: " + req.getMethod() + " " + req.getRequestURI());
-        resp.getWriter().write("Hello from GabSpringDispatchServlet!");
+        //System.out.println("Received request: " + req.getMethod() + " " + req.getRequestURI());
+        //resp.getWriter().write("Hello from GabSpringDispatchServlet!");
+
+        final var url = req.getRequestURI();
+        final var httpMethod = req.getMethod().toUpperCase();
+        final var key = String.format("%s%s", httpMethod, url);
+        final var requestControllerData = ControllersMap.values.get(key);
+
+        GabLogger.log("GabDispatchServlet", String.format("URL: %s (%s) - Handler %s.%s", url, httpMethod, requestControllerData.controllerClass(), requestControllerData.controllerMethod()));
+
+        Object controller;
+        try {
+            GabLogger.log("GabDispatchServlet", "Searching for controller instances");
+            controller = ControllerInstances.instances.get(requestControllerData.controllerClass());
+            if (controller == null) {
+                GabLogger.log("GabDispatchServlet", "Creating new controller instance");
+                controller = Class.forName(requestControllerData.controllerClass()).getDeclaredConstructor().newInstance();
+
+                ControllerInstances.instances.put(requestControllerData.controllerClass(), controller);
+            }
+
+            Method controllerMethod = null;
+            for (Method method : controller.getClass().getMethods()) {
+                if (method.getName().equals(requestControllerData.controllerMethod())) {
+                    controllerMethod = method;
+                    break;
+                }
+            }
+
+            GabLogger.log("GabDispatchServlet", String.format("Invoking controller method %s to handle request", controllerMethod.getName()));
+            PrintWriter out = new PrintWriter(resp.getWriter());
+            out.println(controllerMethod.invoke(controller));
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

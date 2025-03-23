782bc6d454dbbcccc567d35507327950dea2399e
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2025-03-23 02:50:41 -0300

    feat: add Gson para formatacao de saida em json + add tratamento de GabBody

--- src/main/java/br/com/gabspring/web/GabSpringDispatchServlet.java
+++ src/main/java/br/com/gabspring/web/GabSpringDispatchServlet.java
@@ -1,15 +1,20 @@
 package br.com.gabspring.web;
 
+import br.com.gabspring.annotations.GabBody;
 import br.com.gabspring.datastructures.ControllerInstances;
 import br.com.gabspring.datastructures.ControllersMap;
 import br.com.gabspring.util.GabLogger;
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class GabSpringDispatchServlet extends HttpServlet {
 
@@ -20,6 +25,9 @@ public class GabSpringDispatchServlet extends HttpServlet {
         if (req.getRequestURL().toString().endsWith("/favicon.ico")) {
             return;
         }
+
+        PrintWriter out = new PrintWriter(resp.getWriter());
+        Gson gson = new Gson();
 
         //System.out.println("Received request: " + req.getMethod() + " " + req.getRequestURI());
         //resp.getWriter().write("Hello from GabSpringDispatchServlet!");
@@ -50,12 +58,38 @@ public class GabSpringDispatchServlet extends HttpServlet {
                 }
             }
 
+            if (controllerMethod.getParameterCount() > 0) {
+                GabLogger.log("GabDispatchServlet", "Method " + controllerMethod.getName() + " has parameters");
+                Object arg;
+                Parameter parameter = controllerMethod.getParameters()[0];
+                if (parameter.getAnnotations()[0].annotationType().getName().contains(GabBody.class.getName())) {
+                    GabLogger.log("GabDispatchServlet", "Found parameter from request of type " + parameter.getType().getName());
+                    final var body = readBytesFromRequest(req);
+                    GabLogger.log("GabDispatchServlet", "Parameter content: " + body);
+                    arg = gson.fromJson(body, parameter.getType());
+
+                    out.println(gson.toJson(controllerMethod.invoke(controller, arg)));
+                }
+                out.close();
+                return;
+            }
+
             GabLogger.log("GabDispatchServlet", String.format("Invoking controller method %s to handle request", controllerMethod.getName()));
-            PrintWriter out = new PrintWriter(resp.getWriter());
-            out.println(controllerMethod.invoke(controller));
+
+            out.println(gson.toJson(controllerMethod.invoke(controller)));
             out.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String readBytesFromRequest(HttpServletRequest req) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
     }
 }
